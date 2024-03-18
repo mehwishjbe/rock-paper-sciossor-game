2af636ac2b2794ca7f62d19d0a7e34b51e18c112
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-18 12:19:23 +0100

    playGame function added

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choices");
 
+const playGame = (userChoice) => {
+  console.log("user choice = ", userChoice);
+};
+
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
     const userChoice = choice.getAttribute("id");
