094ecb551792632bf0978c7c345b36440ce40037
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-18 12:22:23 +0100

    Math.random function added

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const choices = document.querySelectorAll(".choices");
 
 const genComputerChoice = () => {
   const options = ["rock", "paper", "scissor"];
+  const randomId = Math.floor(Math.random() * 3);
+  return options[randomId];
 };
 
 const playGame = (userChoice) => {
