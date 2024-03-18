dd407460b7c3c18cb309f268a84d3f27c27b6a3e
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-18 12:07:44 +0100

    event listener on divs

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choices");
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        
+    })
+})
