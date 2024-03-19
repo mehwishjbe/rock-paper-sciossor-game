8208047d993f7b8200e5dd83c1009814672f5a72
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-19 14:33:53 +0100

    accesing message div

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
 
 const genComputerChoice = () => {
   const options = ["rock", "paper", "scissor"];
